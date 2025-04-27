688ac64719ec18017477db94e07587f036a9c482
Elias Baeza <baezahuertaelias@gmail.com>
2025-04-26 23:42:13 -0400

    fix update user

--- frontend/src/views/CreateUser.vue
+++ frontend/src/views/CreateUser.vue
@@ -3,11 +3,7 @@
     <h1>{{ isNew ? "Crear Usuario" : "Modificar Usuario" }}</h1>
     <form @submit.prevent="saveUser">
       <InputText v-model="user.username" placeholder="Username" required />
-      <Password
-        v-model="user.password"
-        placeholder="Password"
-        required
-      />
+      <Password v-model="user.password" placeholder="Password" />
       <Dropdown
         v-model="user.userTypeId"
         :options="userTypes"
@@ -17,98 +13,110 @@
         required
       />
       <InputSwitch v-model="user.status" trueValue="true" falseValue="false" />
-      <Button label="Guardar" class="mt-3" type="submit"/>
+      <Button label="Guardar" class="mt-3" type="submit" />
     </form>
   </div>
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
-import { useRouter, useRoute } from 'vue-router'
-import InputText from 'primevue/inputtext'
-import Password from 'primevue/password'
-import Dropdown from 'primevue/dropdown'
-import Button from 'primevue/button'
-import InputSwitch from 'primevue/inputswitch'
-import apiClient from '../axios-config'
+import { ref, onMounted } from "vue";
+import { useRouter, useRoute } from "vue-router";
+import InputText from "primevue/inputtext";
+import Password from "primevue/password";
+import Dropdown from "primevue/dropdown";
+import Button from "primevue/button";
+import InputSwitch from "primevue/inputswitch";
+import apiClient from "../axios-config";
 
-const router = useRouter()
-const route = useRoute()
+const router = useRouter();
+const route = useRoute();
 
 // State to hold user data
 const user = ref({
-  username: '',
-  password: '',
+  username: "",
+  password: "",
   userTypeId: null,
-  status: true
-})
+  status: true,
+});
 
-const isNew = ref(true)
-const userTypes = ref([])
+const isNew = ref(true);
+const userTypes = ref([]);
 
 onMounted(() => {
-  const userId = route.query.id || route.params.id // Use query or params to get the ID
-  console.log('onmounted userid', userId);
-  
+  const userId = route.query.id || route.params.id; // Use query or params to get the ID
+  console.log("onmounted userid", userId);
+
   if (userId) {
-    isNew.value = false
-    fetchUser(userId)
+    isNew.value = false;
+    fetchUser(userId);
   }
-  fetchUserTypes()
-})
+  fetchUserTypes();
+});
 
 const fetchUser = async (id) => {
   try {
-    const response = await apiClient.get(`/users/${id}`)
- 
+    const response = await apiClient.get(`/users/${id}`);
+
     if (response.status === 200) {
       console.log(`fetchUser with ID=${id}`, response.data.user);
-      
-      user.value = response.data.user
+
+      response.data.user.password = "";
+
+      user.value = response.data.user;
     }
   } catch (error) {
-    console.error('Failed to fetch user:', error)
+    console.error("Failed to fetch user:", error);
   }
-}
+};
 
 const fetchUserTypes = async () => {
   try {
-    
-    const response = await apiClient.get('users/types/usertypes')
-    
+    const response = await apiClient.get("users/types/usertypes");
+
     if (response.status === 200) {
-      userTypes.value = response.data.userTypes
+      userTypes.value = response.data.userTypes;
     }
   } catch (error) {
-    console.error('Failed to fetch user types:', error)
+    console.error("Failed to fetch user types:", error);
   }
-}
+};
 
 const saveUser = async () => {
   try {
-    const userData = { ...user.value }
+    const userData = { ...user.value };
 
     // Hash the password before saving if it's a new user or password is changed
     if (isNew.value || user.value.password) {
-      userData.password = await bcrypt.hash(user.value.password, 10)
+      //userData.password = await bcrypt.hash(user.value.password, 10)
     } else {
-      delete userData.password // Remove password field for update if not changed
+      //delete userData.password; // Remove password field for update if not changed
     }
 
-    let response
+    //TODO: Tomar la respuesta cuando es status=400 y mostrar msj error
+
+    if (!isNew && userData.password.length < 8) {
+      alert("passwd debe ser mayor a 8");
+      return null;
+    }
+    if (!isNew && userData.userTypeId === null) {
+      alert("elegir tipo de usuario");
+      return null;
+    }
+
+    let response;
     if (isNew.value) {
-      response = await apiClient.post('/users', userData)
+      response = await apiClient.post("/users", userData);
     } else {
-      response = await apiClient.put(`/users/${user.value.id}`, userData)
+      response = await apiClient.put(`/users/${user.value.id}`, userData);
     }
 
     if (response.status === 200 || response.status === 201) {
-      router.push({ name: 'listUser' })
+      router.push({ name: "Listado usuarios" });
     }
   } catch (error) {
-    console.error('Failed to save user:', error)
+    console.error("Failed to save user:", error);
   }
-}
+};
 </script>
 
 <style scoped>
