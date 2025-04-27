4dea92c74ca8f49e23aa8dba781757d207a85281
Elias Baeza <baezahuertaelias@gmail.com>
2025-04-27 01:33:24 -0400

    preloadDataDB

--- backend/src/bin/preloadData.js
+++ backend/src/bin/preloadData.js
@@ -2,39 +2,42 @@ const bcrypt = require("bcrypt");
 const { Users, UserTypes } = require("../models");
 
 module.exports = {
-  async fun() {
-    // Hash the password before storing it
-    const hashedPassword = await bcrypt.hash(password, 10);
-
+  async insertBasicData() {
     try {
-      const user = await Users.create([
-        {
-          username: "elias",
-          password: hashedPassword,
-          userTypeId: 1,
-        },
-        {
-          username: "javi",
-          password: hashedPassword,
-          userTypeId: 2,
-        },
-      ]);
 
-      console.log("User created:", user);
+      console.log('================= TYPE USER');
 
-      const typeUser = await UserTypes.create(
-        {
+      const typeUser = await UserTypes.bulkCreate(
+        [{
           id: 1,
           type: "Admin",
         },
         {
           id: 2,
-          type: "Bendedor",
-        }
+          type: "Vendedor",
+        }]
       );
 
       console.log("TypeUser created:", typeUser);
-      
+
+      console.log('================= USER');
+      const password = '123';
+      // Hash the password before storing it
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      const user = await Users.create({
+        id: 1,
+        username: "elias",
+        password: hashedPassword,
+        userTypeId: 1,
+        status: true
+      })
+
+
+      console.log("User created:", user);
+
+
+
     } catch (error) {
       console.error("Error creating user:", error);
     }
