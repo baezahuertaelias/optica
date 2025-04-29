251459a7e62fb31596131bed25b57e3987386347
Elias Baeza <baezahuertaelias@gmail.com>
2025-04-29 15:31:09 -0400

    Agregado nginx

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,18 +2,18 @@
 
 module.exports = {
     development: {
-      username: process.env.DB_USER.replaceAll('\r', ''),
-      password: process.env.DB_PASSWORD.replaceAll('\r', ''),
-      database: process.env.DB_NAME.replaceAll('\r', ''),
-      host: process.env.DB_HOST.replaceAll('\r', ''),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
       dialect: 'postgres',
       logging: false
     },
     production: {
-      username: process.env.DB_USER.replaceAll('\r', ''),
-      password: process.env.DB_PASSWORD.replaceAll('\r', ''),
-      database: process.env.DB_NAME.replaceAll('\r', ''),
-      host: process.env.DB_HOST.replaceAll('\r', ''),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      host: process.env.DB_HOST,
       dialect: 'postgres',
       logging: false
     }
